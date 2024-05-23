54eb4f3e3424a1be311917f79c22d154c3f83fae
Maryam Abdi <maryamabdi9776@gmail.com>
2024-05-23 08:28:56 +0330

    :tada: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relativ z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Tech />
+      <Experience />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
